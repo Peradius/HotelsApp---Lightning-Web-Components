69068895c291aef24f20752c91928ad07719a61d
Peradius <peradius@gmail.com>
2019-08-13 13:39:20 +0200

    Added lwcUserMenu

--- lwc/lwcUserMenu/lwcUserMenu.js
+++ lwc/lwcUserMenu/lwcUserMenu.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+import { LightningElement, track } from 'lwc';
+import getReservations from '@salesforce/apex/LwcUserMenuController.getReservations';
 
-export default class LwcUserMenu extends LightningElement {}
+export default class LwcUserMenu extends LightningElement {
+    @track guest;
+    @track loggedIn = false;
+    @track reservations;
+    @track showBillingPage = false;
+    @track showReservationDetails = false;
+
+    handleLogin(event) {
+        this.guest = event.detail;
+        this.loggedIn = true;
+        this.queryReservations();
+    }
+
+    queryReservations() {
+        getReservations({
+            guestId: this.guest.Id
+        })
+        .then(result => {
+            console.log('Reservations retrieved successfully!');
+            this.reservations = result;
+        })
+        .catch(error => {
+            console.log('Error retrieving reservations!');
+            console.log(error);
+        })
+    }
+}
